7da3bfb52d5da66494dee199d6b548eab0dbb13d
adam clark <aclark64@gmail.com>
2017-05-26 14:12:19 -0600

    import PropTypes from 'prop-types';

--- generators/app/templates/_statefullcomponent.js
+++ generators/app/templates/_statefullcomponent.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 if (process.env.BROWSER) {
   require('./style.scss');
